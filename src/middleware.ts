d099a8c347411dbdb96d9a22233e8b2db31d9f7e
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-04-03 07:59:45 +0600

    Post Router Introduced

---
+++ src/middleware.ts
@@ -0,0 +1,6 @@
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export default withClerkMiddleware(() => {
+  return NextResponse.next();
+});
