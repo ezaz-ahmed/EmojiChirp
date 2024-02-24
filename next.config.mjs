df08ee4e7f2369fc81de026421afecffb5f85eb1
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-02-25 03:05:23 +0600

    📁 feat[file-upload]: Configure file uploading with Uploading Thing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uploadthing.com',
+        port: '',
+        pathname: '/ezaz/**',
+      },
+    ],
+  },
+  reactStrictMode: false
+};
 
 export default nextConfig;
