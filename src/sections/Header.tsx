1755219b505dd38603c894bb3ae2c9a4baed8027
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-11-29 22:08:53 +0600

    🐛 fix: update alias and manage Node engine version

---
+++ src/sections/Header.tsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <a href=''>
+          <img
+            src='/images/xorik.svg'
+            width={115}
+            height={55}
+            alt='Xorik Logo'
+          />
+        </a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
