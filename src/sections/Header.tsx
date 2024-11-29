a72fbce1e6a4a2570819b619372c632ceaeec9e4
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-11-30 00:41:06 +0600

    ✨ feat: add smooth scrolling to header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,9 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link as LinkScroll } from 'react-scroll'
+import { debounce } from 'lodash'
 import clsx from 'clsx'
 
 const Header = () => {
+  const [hasScrolled, setHasScrolled] = useState(false)
   const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = debounce(() => {
+      setHasScrolled(window.scrollY > 32)
+    }, 100)
+
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+      handleScroll.cancel()
+    }
+  }, [])
 
   const NavLink = ({ title }: { title: string }) => (
     <LinkScroll
@@ -22,7 +36,8 @@ const Header = () => {
   return (
     <header
       className={clsx(
-        'fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4  bg-black-100 backdrop-blur-[8px]'
+        'fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4',
+        hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]'
       )}
     >
       <div className='container flex h-14 items-center max-lg:px-5'>
